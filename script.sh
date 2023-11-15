bd79f354d5ca10f85ff540af018dbc3a26ad8051
hasanaltin <62545852+hasanaltin@users.noreply.github.com>
2023-11-15 16:21:42 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,36 +22,3 @@ fi
 pwd
 ls
 cd ~/Downloads
-
-echo "TeamViewerQS indiriliyor..."
-
-wget https://www.erybilisim.com/wp-content/vmwarehorizon/TeamViewerQS.tar.gz
-
-echo "TeamViewerQS cikartiliyor ve kuruluyor..."
-
-sudo tar -xvzf /home/ery/Downloads/TeamViewerQS.tar.gz
-
-echo "TeamViewerQS kisayol olusturuluyor..."
-
-cp /home/ery/Downloads/TeamViewerQS/teamviewerqs/teamviewer.desktop ~/Desktop
-gio set ~/Desktop/teamviewer.desktop metadata::trusted true
-chmod a+x ~/Desktop/teamviewer.desktop
-
-echo "TeamViewerQS kurulumu tamamlandi."
-
-pwd
-
-echo "VMware Horizon Client indiriliyor..."
-
-https://download3.vmware.com/software/CART24FQ3_LIN64_2309/VMware-Horizon-Client-2309-8.11.0-22660930.x64.bundle
-
-echo "VMware Horizon Client cikartiliyor ve kuruluyor..."
-
-echo "VMware Horizon Client kisayol olusturuluyor..."
-cp /usr/share/applications/vmware-view.desktop ~/Desktop
-gio set ~/Desktop/vmware-view.desktop metadata::trusted true
-chmod a+x ~/Desktop/vmware-view.desktop
-
-echo "VMware Horizon Client  kurulumu tamamlandi."
-
-pwd
